a00c5a20f8610334a1437c0b2d799e45e6d2de5d
SgtRaider <gth4wggvwb@privaterelay.appleid.com>
2026-05-05 22:08:24 +0200

    Revert "fix(pdf): anchos de columna explicitos en log de vuelo (12 cols caben en A4)"
    This reverts commit 9eb62b6f65b45388d157bd011c0f98f0edf1dc97.

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * Para forzar invalidación al desplegar nueva versión, sube CACHE_VERSION.
  */
 
-const CACHE_VERSION = 'tsagestor-v23';
+const CACHE_VERSION = 'tsagestor-v24';
 const SHELL_CACHE   = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
